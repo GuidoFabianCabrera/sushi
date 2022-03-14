4b156ddbcba3276a205efeafbaf30dd9ba777595
GuidoFC <guidofabiancabrera@hotmail.com>
2022-03-14 16:32:13 -0300

    6f91228

---
+++ _nuxt/static/1647286328/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsushi\u002F_nuxt\u002Fstatic\u002F1647286328",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsushi\u002F",assetsPath:"\u002Fsushi\u002F_nuxt\u002F",cdnURL:a}}}}(null));
